075088461c6ed06029846d6d4288d991de3f7321
Rainnie-oo7 <65252533+Rainnie-oo7@users.noreply.github.com>
2024-11-28 17:46:42 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,19 @@
-import os.path as osp
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision.transforms as transforms
 import torchvision
-from torch.utils.data import DataLoader
+from torchvision.io import read_image
+
 from Mydatasetnuryourright import Mydataset
+
+from engine import train_one_epoch, evaluate
+import utils
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
 import torchvision.transforms.v2 as T
+from get_model_instance_segmentation import *   #hier is: from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import matplotlib.pyplot as plt
+from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks
+
 
 
 def get_transform(train):
@@ -16,92 +23,211 @@ def get_transform(train):
     transforms.append(T.ToDtype(torch.float, scale=True))
     transforms.append(T.ToPureTensor())
     return T.Compose(transforms)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-in_channel = 3
-num_classes = 10
-learning_rate = 1e-3
-batch_size = 1
-num_epochs = 1
-path = osp.normpath(osp.join(osp.dirname(__file__), "data"))
-
-dataset = Mydataset(torch.utils.data.Dataset, get_transform(train=True))
-train_set, test_set = torch.utils.data.random_split(dataset, [230, 70])
-train_loader = torch.utils.data.DataLoader(
-    dataset=train_set,
-    batch_size=batch_size,
-    shuffle=True
-)
-test_loader = torch.utils.data.DataLoader(
-    dataset=test_set,
-    batch_size=batch_size,
-    shuffle=False
-)
-
+###################APPROACH 1#########################start from a model pre-trained on COCO Common Objects in Context
+# load a model pre-trained on COCO
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
-model = model.to(device)
 
-criterion = nn.CrossEntropyLoss
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+# replace the classifier with a new one, that has
+# num_classes which is user-defined
+num_classes = 2  # 1 class (person) + background
+# get number of input features for the classifier
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+# replace the pre-trained head with a new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+#Approach 2 nicht nehmen. (nicht probiert):
+
+"""
+#Approach 2 wechselt das fasterrcn durch mobilenet aus!!!
+###################APPROACH 2######################### - Modifying the model to add a different backbone
+#
+# # load a pre-trained model for classification and return
+# # only the features
+# backbone = torchvision.models.mobilenet_v2(weights="DEFAULT").features
+# # ``FasterRCNN`` needs to know the number of
+# # output channels in a backbone. For mobilenet_v2, it's 1280
+# # so we need to add it here
+# backbone.out_channels = 1280
+#
+# # let's make the RPN generate 5 x 3 anchors per spatial
+# # location, with 5 different sizes and 3 different aspect
+# # ratios. We have a Tuple[Tuple[int]] because each feature
+# # map could potentially have different sizes and
+# # aspect ratios
+# anchor_generator = AnchorGenerator(
+#     sizes=((32, 64, 128, 256, 512),),
+#     aspect_ratios=((0.5, 1.0, 2.0),)
+# )
+#
+# # let's define what are the feature maps that we will
+# # use to perform the region of interest cropping, as well as
+# # the size of the crop after rescaling.
+# # if your backbone returns a Tensor, featmap_names is expected to
+# # be [0]. More generally, the backbone should return an
+# # ``OrderedDict[Tensor]``, and in ``featmap_names`` you can choose which
+# # feature maps to use.
+# roi_pooler = torchvision.ops.MultiScaleRoIAlign(
+#     featmap_names=['0'],
+#     output_size=7,
+#     sampling_ratio=2
+# )
+#
+# # put the pieces together inside a Faster-RCNN model
+# model = FasterRCNN(
+#     backbone,
+#     num_classes=2,
+#     rpn_anchor_generator=anchor_generator,
+#     box_roi_pool=roi_pooler
+# )
+"""
+
+#########################################
+
+#hol TEST:
+
+"""
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+dataset = PennFudanDataset('data/PennFudanPed', get_transform(train=True))
+data_loader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=2,
+    shuffle=True,
+    collate_fn=utils.collate_fn
+)
+
+# For Training
+images, targets = next(iter(data_loader))
+images = list(image for image in images)
+targets = [{k: v for k, v in t.items()} for t in targets]
+output = model(images, targets)  # Returns losses and detections
+print("nikolaus")
+print(output)
+
+# For inference
+model.eval()
+x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+predictions = model(x)  # Returns predictions
+print("drei mal schwarzer kater")
+print(predictions[0])
+"""
+
+#Nachdem Testing:
+
+"""
+# train on the GPU or on the CPU, if a GPU is not available
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# our dataset has two classes only - background and person
+num_classes = 2
+# use our dataset and defined transformations
+dataset = Mydataset(torch.utils.data.Dataset, get_transform(train=True))
+dataset_test = Mydataset(torch.utils.data.Dataset, get_transform(train=False))
+
+
+# split the dataset in train and test set
+indices = torch.randperm(len(dataset)).tolist()
+dataset = torch.utils.data.Subset(dataset, indices[:230])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-70:])
+
+# define training and validation data loaders
+data_loader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=1,
+    shuffle=True,
+    collate_fn=utils.collate_fn
+)
+
+data_loader_test = torch.utils.data.DataLoader(
+    dataset_test,
+    batch_size=1,
+    shuffle=False,
+    collate_fn=utils.collate_fn
+)
+
+# get the model using our helper function
+model = get_model_instance_segmentation(num_classes)
+
+# move model to the right device
+model.to(device)
+
+# construct an optimizer
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(
+    params,
+    lr=0.005,
+    momentum=0.9,
+    weight_decay=0.0005
+)
+
+# and a learning rate scheduler
+lr_scheduler = torch.optim.lr_scheduler.StepLR(
+    optimizer,
+    step_size=3,
+    gamma=0.1
+)
+
+# let's train it just for 2 epochs
+num_epochs = 2
 
 for epoch in range(num_epochs):
-    total_loss = []
+    # train for one epoch, printing every 10 iterations
+    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+    # update the learning rate
+    lr_scheduler.step()
+    # evaluate on the test dataset
+    evaluate(model, data_loader_test, device=device)
 
-    for batch_idx, (img, targets) in enumerate(train_loader):
+print("That's it!")
+torch.save(model, "model.pth")
+torch.save(model.state_dict(), "model_weights.pth")
+"""
+##
 
-        img = img.to(device=device)
-        targets = [{k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in targets.items()}]
+"""
+Abgeschlossen, funktioniert (Objekt Detektion) Kleinere Areas 32² (<1024 px²)haben eine bessere Detektion erbracht 0.85 als größere Areas >96² (>9216²)
+"""
 
+# Anwendung unbekanntesBild
 
-        # Forward-Pass
-        loss_dict = model(img, targets)
-        losses = sum(loss for loss in loss_dict.values())  # Summe aller Verluste
+# # Speichern
+# torch.save({
+#     'model_state_dict': model.state_dict(),
+#     'optimizer_state_dict': optimizer.state_dict(),
+# }, "checkpoint.pth")
+#
+# # Laden
+# checkpoint = torch.load("checkpoint.pth")
+# model.load_state_dict(checkpoint['model_state_dict'])
+# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-        total_loss.append(losses.item())
+image = read_image("data/CARDS_COURTYARD_S_H/frame_0667.jpg")
+eval_transform = get_transform(train=False)
 
-        # Backward und Optimierung
-        optimizer.zero_grad()
-        losses.backward()
-        optimizer.step()
-
-    print(f'Cost at epoch {epoch} is {sum(total_loss)/len(total_loss)}')
-
-def check_accuracy(loader, model):
-    num_correct = 0
-    num_samples = 0
-    model.eval()
-
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device)
-            y = [{k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in y.items()}]
-
-            scores = model(x)
-            predictions = [score['labels'] for score in scores]  # Extract labels from predictions
-
-            for pred, target in zip(predictions, y):
-                # Assuming 'labels' is in y
-                num_correct += (pred == target['labels']).sum().item()
-                num_samples += pred.size(0)
-
-        accuracy = float(num_correct) / float(num_samples) * 100
-        print(f"Got {num_correct} / {num_samples} correct with accuracy {accuracy:.2f} %")
-
-    model.train()
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+model = torch.load("model.pth")
+model.load_state_dict(torch.load("model_weights.pth", map_location=torch.device('cuda')))
+model.eval()  # Wechsel in den Evaluierungsmodus, falls nötig
 
 
-print("checking accuracy on Training set")
-check_accuracy(train_loader, model)
-print("checking accuracy on Test set")
-check_accuracy(test_loader, model)
+model.eval()
+with torch.no_grad():
+    x = eval_transform(image)
+    # convert RGBA -> RGB and move to device
+    x = x[:3, ...].to(device)
+    predictions = model([x, ])
+    pred = predictions[0]
 
 
+image = (255.0 * (image - image.min()) / (image.max() - image.min())).to(torch.uint8)
+image = image[:3, ...]
+pred_labels = [f"yright: {score:.3f}" for label, score in zip(pred["labels"], pred["scores"])]
+pred_boxes = pred["boxes"].long()
+output_image = draw_bounding_boxes(image, pred_boxes, pred_labels, colors="red")
+
+masks = (pred["masks"] > 0.7).squeeze(1)
+output_image = draw_segmentation_masks(output_image, masks, alpha=0.5, colors="blue")
 
 
-
-
-
-
-
-
+plt.figure(figsize=(12, 12))
+plt.imshow(output_image.permute(1, 2, 0))
+plt.show()
